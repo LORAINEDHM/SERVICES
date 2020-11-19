1c29c17710eddd2b30d0e5d209a5cfc6f151cd09
Loraine Duhamel <lduhamel@shi-r10-p7.s19.be>
2020-11-19 18:22:49 +0100

    brol

---
+++ srcs/mysql/srcs/start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#mysql_install_db --user=root --datadir=/var/lib/mysql
+#/usr/bin/mysqld --user=root --datadir=/var/lib/mysqld --bootstrap <<EOF
+
+#FLUSH PRIVILEGES;
+#CREATE DATABASE wordpress;
+#CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+#GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'%' IDENTIFIED BY 'root';
+#FLUSH PRIVILEGES;
+#EOF
+
+#rc-service mariadb restart
+
+
+mysql_install_db --user=root --ldata=/var/lib/mysql
+cat > /tmp/sql << eof
+CREATE DATABASE wordpress;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+eof
---
+++ srcs/wordpress/srcs/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+nginx -g "daemon off;"
+#nginx -g "daemon off;"
