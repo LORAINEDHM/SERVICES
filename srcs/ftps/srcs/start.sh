fba9da8492fc5af1dd6aed2a99f80641ac8d40e7
Simon Simon <ssimon@shi-r9-p4.s19.be>
2020-12-16 16:14:32 +0100

    all works ; need to verify everything before pushing

--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -1,4 +1,7 @@
 #(telegraf conf &) && /usr/sbin/vsftpd -Y 2 -p 30000:30009 -P 192.168.99.114
-(telegraf conf &) && /usr/sbin/vsftpd -Y 2 -p 21000:21000 -P 192.168.99.114
-# /usr/sbin/pure-ftpd -Y 2 -p 30000:30009 -P 192.168.99.114
+# (telegraf conf &) && /usr/sbin/vsftpd -Y 2 -p 21000:21000 -P 192.168.99.114
+# # /usr/sbin/pure-ftpd -Y 2 -p 30000:30009 -P 192.168.99.114
+# tail -f /dev/null
+# (telegraf conf &) && vsftpd /etc/vsftpd/vsftpd.conf
+(telegraf conf &) && /usr/sbin/pure-ftpd -Y 2 -p 30000:30009 -P 192.168.99.114
 tail -f /dev/null
