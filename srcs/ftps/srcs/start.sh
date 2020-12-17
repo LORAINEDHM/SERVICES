87206000726c55628376fb280dc3e514d7f43aa0
Simon Simon <ssimon@shi-r9-p4.s19.be>
2020-12-17 17:12:19 +0100

    sharep ip added ; one unique IP address now : 110

--- srcs/ftps/srcs/start.sh
+++ srcs/ftps/srcs/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(telegraf conf &) && /usr/sbin/pure-ftpd -Y 2 -p 30000:30009 -P 192.168.99.114
+(telegraf conf &) && /usr/sbin/pure-ftpd -Y 2 -p 30000:30009 -P 192.168.99.110
 tail -f /dev/null
